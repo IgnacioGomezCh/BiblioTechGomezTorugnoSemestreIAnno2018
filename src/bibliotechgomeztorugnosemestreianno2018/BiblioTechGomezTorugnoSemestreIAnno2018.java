4184eec5bde91ec277c24a6428258cfffc7aaf5c
Ignacio Gomez <IgnacioGomezCh@users.noreply.github.com>
2018-02-17 16:27:05 -0600

    Start of project

---
+++ src/bibliotechgomeztorugnosemestreianno2018/BiblioTechGomezTorugnoSemestreIAnno2018.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bibliotechgomeztorugnosemestreianno2018;
+
+/**
+ *
+ * @author Nacho
+ */
+public class BiblioTechGomezTorugnoSemestreIAnno2018 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //New project
+        // TODO code application logic here
+    }
+    
+}
